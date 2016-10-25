88a8cfa859d03fd2c201f2ee952a7bf591098ad3
Rachel Rattay <ror5226@psu.edu>
2016-10-25 15:15:22 -0400

    SpatialMapping Integrated

---
+++ MRTSeniorDesignTeam4/Assets/BeginScan.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using HoloToolkit.Unity;
+
+public class BeginScan : MonoBehaviour {
+
+    SpatialMappingManager spatialMappingManager; 
+
+	// Use this for initialization
+	void Start () {
+
+        spatialMappingManager = SpatialMappingManager.Instance;
+        spatialMappingManager.StartObserver();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
