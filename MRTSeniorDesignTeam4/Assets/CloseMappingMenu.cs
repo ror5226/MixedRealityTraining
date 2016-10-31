1959ae8272cee37d6bfe49a0669d59f61cc1a979
Mason Toy <mason.toy@gmail.com>
2016-10-30 20:05:17 -0400

    UI at Start
    Testing to see if the UI will work in the emulator.

---
+++ MRTSeniorDesignTeam4/Assets/CloseMappingMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseMappingMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void OnSelect() {
+        Destroy(this.gameObject);
+    }
+}
