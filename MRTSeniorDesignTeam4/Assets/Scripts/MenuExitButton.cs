6dfd374f34051cc1d0e7641d5d42866d35e55ed4
Mason Toy <mason.toy@gmail.com>
2016-11-08 14:19:10 -0500

    Damage Information Panel Created
    Finished the damage information panel. This panel will be used to
    display the information about an object when a damage asset is tapped.

---
+++ MRTSeniorDesignTeam4/Assets/Scripts/MenuExitButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuExitButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnSelect() {
+        this.transform.parent.gameObject.SetActive(false);
+    }
+
+}
