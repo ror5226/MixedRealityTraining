e8c6a9e5309706de62eaa84f5697f55fa990d33a
Mason Toy <mason.toy@gmail.com>
2016-11-10 13:11:52 -0500

    AssessmentPanel update
    DamageInfo Panel now displays the assessment panel when you tap the take
    assessment button. The assessment panel exit button also exits the panel
    and then brings up the damage info panel again.

---
+++ MRTSeniorDesignTeam4/Assets/Scripts/AssessmentExitButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssessmentExitButton : MonoBehaviour {
+
+    void OnSelect() {
+        GameObject damageInfo = this.transform.parent.transform.parent.FindChild("DamageInfo").gameObject;
+        damageInfo.SetActive(true);
+        this.transform.parent.gameObject.SetActive(false);
+    }
+}
---
+++ MRTSeniorDesignTeam4/Assets/Scripts/OpenSecondPanel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenSecondPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnSelect() {
+        GameObject assessmentPanel = this.transform.parent.transform.parent.FindChild("AssessmentPanel").gameObject;
+        GameObject damageCanvas = this.transform.parent.gameObject;
+
+        //
+        //  First set the assessmentPanel to be visable
+        //  Then set the assessmentPanel's position to be where the damage info's position.
+        //  then hide damageInfoPanel.
+        //
+        assessmentPanel.SetActive(true);
+        assessmentPanel.transform.position = damageCanvas.transform.position;
+        this.transform.parent.gameObject.SetActive(false);
+    }
+}
