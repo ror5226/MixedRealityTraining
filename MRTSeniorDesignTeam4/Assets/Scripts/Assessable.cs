7d68110c90f8eb9975340dc86f2f67a397bc3cab
Rachel Rattay <ror5226@psu.edu>
2017-01-26 16:35:44 -0500

    Defining Placement Types

---
+++ MRTSeniorDesignTeam4/Assets/Scripts/Assessable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlacementPosition
+{
+    Floor = 1,
+    HighWall = 2,
+    MidWall = 3,
+    LowWall = 4,
+    WallFloor = 5
+}
+
+public class Assessable : MonoBehaviour {
+
+    [Tooltip("Type of plane that the object has been classified as.")]
+    public PlacementPosition placement = PlacementPosition.MidWall;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- MRTSeniorDesignTeam4/Assets/Scripts/RoomAssetManager.cs
+++ MRTSeniorDesignTeam4/Assets/Scripts/RoomAssetManager.cs
@@ -4,8 +4,50 @@ using UnityEngine;
 
 public class RoomAssetManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+
+    [Tooltip("A collection of assessable room objects to generate in the world.")]
+    public List<GameObject> spaceObjectPrefabs;
+
+    public void GenerateItemsInWorld(List<GameObject> horizontalSurfaces, List<GameObject> verticalSurfaces)
+    {
+        List<GameObject> floorObjects = new List<GameObject>();
+        List<GameObject> highWallObjects = new List<GameObject>();
+        List<GameObject> midWallObjects = new List<GameObject>();
+        List<GameObject> lowWallObjects = new List<GameObject>();
+        List<GameObject> wallFloorObjects = new List<GameObject>();
+
+        foreach (GameObject spacePrefab in spaceObjectPrefabs)
+        {
+            Assessable assessObject = spacePrefab.GetComponent<Assessable>();
+            if (assessObject.placement == PlacementPosition.Floor)
+            {
+                floorObjects.Add(spacePrefab);
+            }
+            else if (assessObject.placement == PlacementPosition.LowWall)
+            {
+                lowWallObjects.Add(spacePrefab);
+            }
+            else
+            {
+                midWallObjects.Add(spacePrefab);
+            }
+        }
+        /*
+        if (horizontalObjects.Count > 0)
+        {
+            CreateSpaceObjects(horizontalObjects, horizontalSurfaces, PlacementSurfaces.Horizontal);
+        }
+
+        if (verticalObjects.Count > 0)
+        {
+            CreateSpaceObjects(verticalObjects, verticalSurfaces, PlacementSurfaces.Vertical);
+        }
+        */
+    }
+
+   
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
