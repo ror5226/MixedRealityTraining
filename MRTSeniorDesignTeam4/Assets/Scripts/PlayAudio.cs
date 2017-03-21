7334baa623dbdb73c7f43c404410e586ec922b8e
wsemmel <wse5014@psu.edu>
2017-03-21 15:04:45 -0400

    Text To Speech
    The Text To Speech Test scene

---
+++ MRTSeniorDesignTeam4/Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,21 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour {
+    public TextToSpeechManager TextToSpeech;
+   
+    public void Play() {
+        // Get the name
+        var voiceName = System.Enum.GetName(typeof(TextToSpeechVoice), TextToSpeech.Voice);
+
+        // Create message
+        var msg = string.Format("This is the {0} voice. It should sound like it's coming from the object you clicked. Feel free to walk around and listen from different angles.", voiceName);
+
+        // Speak message
+        TextToSpeech.SpeakText(msg);
+    }
+	
+	
+}
